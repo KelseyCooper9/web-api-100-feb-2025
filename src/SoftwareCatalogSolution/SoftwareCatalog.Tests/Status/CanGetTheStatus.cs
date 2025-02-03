4ae074d6447a35962ce817c057b4352b025d24de
KelseyCooper9 <KelseyJCooper89@gmail.com>
2025-02-03 16:31:33 -0500

    done with day 1

---
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Tests/Status/CanGetTheStatus.cs
@@ -0,0 +1,19 @@
+﻿using Alba;
+
+namespace SoftwareCatalog.Tests.Status;
+// Have to be public classes.  And they have to extend* ControllerBase
+public class CanGetTheSTatus
+{
+    [Fact]
+    public async Task GetsA200WhenGettingTheStatus()
+    {
+        var host = await AlbaHost.For<Program>();
+        await host.Scenario(api =>
+        {
+            api.Get.Url("/status");
+            api.StatusCodeShouldBeOk();
+        });
+    }
+
+
+}
