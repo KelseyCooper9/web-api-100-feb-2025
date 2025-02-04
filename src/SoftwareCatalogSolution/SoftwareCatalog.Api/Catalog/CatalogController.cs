d5ac5e344b771d1200c7291f4915b095527343b7
KelseyCooper9 <KelseyJCooper89@gmail.com>
2025-02-04 14:38:11 -0500

    add catalog controller

---
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Api/Catalog/CatalogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareCatalog.Api.Catalog;
+public class CatalogController : ControllerBase
+{
+    [HttpPost("/vendors/microsoft/opensource")]
+    public async Task<ActionResult> AddItemToCatalogAsync([FromBody] CatalogItemRequestModel request)
+    {
+        return Ok(request);
+    }
+}
+
+public record CatalogItemRequestModel
+{
+    public string Name { get; set; } = string.Empty;
+}
